1aacc9f258ca436eccf0e745565c2c992801ed17
sunqi <sunqi@reach.store>
2022-01-13 17:06:38 +0800

    添加 环境env

--- src/apis/configuration.ts
+++ src/apis/configuration.ts
@@ -1,5 +1,8 @@
-import { hooks, createConfiguration } from '@midwayjs/hooks';
-import bodyParser from 'koa-bodyparser';
+import { hooks, createConfiguration } from "@midwayjs/hooks";
+import bodyParser from "koa-bodyparser";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 export default createConfiguration({
   imports: [
